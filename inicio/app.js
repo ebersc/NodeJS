0968bbad4246fc9885fd760cacd3c07372747097
ebersc <eberson.santoscosme@gmail.com>
2024-08-23 17:06:25 -0300

    Primeiro código

---
+++ inicio/app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = 'www.guiadoprogramador.com';
+
+console.log('Hello World!');
+console.log('Meu nome é Eberson');
+console.log('E eu estou aprendo Node.js com o guia do programador');
+
+if(mostrarSite){
+    console.log(site)
+}
