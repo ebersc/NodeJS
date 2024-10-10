4bfcf6535e8de053f8d838a639be6476cec4df87
Eber <eberson.santoscosme@gmail.com>
2024-10-10 17:14:54 -0300

    estrutura de repetição

--- projeto_perguntas/index.js
+++ projeto_perguntas/index.js
@@ -7,10 +7,21 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false;
+
+    var produtos = [
+        {nome: "doritos", preco: 3.14},
+        {nome: "coca-cola", preco: 5},
+        {nome: "leite", preco: 1.45},
+        {nome: "carne", preco: 15},
+        {nome: "sprite", preco: 6}
+
+    ];
+
     res.render("index", {
         nome: nome,
         lang: lang,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
