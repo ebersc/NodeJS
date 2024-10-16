a85271554e2122b54cd3b99b601de9382ae9ec9b
Eber <eberson.santoscosme@gmail.com>
2024-10-16 15:47:04 -0300

    install bootstrap

--- projeto_perguntas/index.js
+++ projeto_perguntas/index.js
@@ -4,26 +4,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "doritos", preco: 3.14},
-        {nome: "coca-cola", preco: 5},
-        {nome: "leite", preco: 1.45},
-        {nome: "carne", preco: 15},
-        {nome: "sprite", preco: 6}
-
-    ];
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.listen(8080, () => {
