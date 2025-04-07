ab1770a5a6a14e682afc8f8fccabd9df4118d866
Eber <eberson.santoscosme@gmail.com>
2025-04-07 15:59:29 -0300

    Finalização do projeto de perguntas

--- projeto_perguntas/index.js
+++ projeto_perguntas/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
 connection
     .authenticate()
@@ -20,15 +21,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    Pergunta.findAll().then(perguntas => {
-
+    Pergunta.findAll({raw: true, order:[
+        ['id', 'DESC']
+    ]}).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
     });
-    res.render("index");
 });
 
 app.get("/perguntar", (req,res) => {
     res.render("perguntar");
-});  
+});
 
 app.post("/salvarpergunta", (req, res) => {
 
@@ -42,6 +46,43 @@ app.post("/salvarpergunta", (req, res) => {
         res.redirect('/');
     });
     // res.send("Formulário recebido! titulo: " + titulo + " descricao: " + descricao);
+});
+
+app.get("/pergunta/:id", (req, res) => {
+    let id = req.params.id;
+
+    Pergunta.findOne({raw: true,
+        where:{
+            id:id
+        }
+    }).then(pergunta => {
+        if(pergunta != undefined){
+
+            Resposta.findAll({
+                raw: true,
+                where: {perguntaId: pergunta.id},
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/responder", (req, res) =>{
+    let corpo = req.body.corpo;
+    let perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
+    });
 });
 
 app.listen(8080, () => {
